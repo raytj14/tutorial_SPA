e9165251f408cc3fb9b6542c4cb979701a06373b
raytj14 <reitojo2001@gmail.com>
2022-05-03 09:08:00 +0900

    install django==2.2.*

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\raytj\Documents\tutorial10\tutorial_SPA\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
