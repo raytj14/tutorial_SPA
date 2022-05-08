a66874a164371c17b7ea2557267e1cf9a2ff803a
raytj14 <reitojo2001@gmail.com>
2022-05-08 10:14:08 +0900

    modify config urls

--- mysite/config/urls.py
+++ mysite/config/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('api-auth/', include('rest_framework.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
     path('api/v1/', include('apiv1.urls')),
+    re_path('', RedirectView.as_view(url='/')),
 ]
