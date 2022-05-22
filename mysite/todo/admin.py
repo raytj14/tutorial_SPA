14d3f32ee5f104f3046d9de95699333d72bad1da
raytj14 <reitojo2001@gmail.com>
2022-05-22 16:43:13 +0900

    readonlyfields setting is deleated

--- mysite/todo/admin.py
+++ mysite/todo/admin.py
@@ -5,6 +5,5 @@ from .models import Idea
 class IdeaModelAdmin(admin.ModelAdmin):
     list_display = ('idea', 'remind', 'status', 'created_at', 'updated_at')
     ordering = ('-created_at',)
-    readonly_fields = ('create_at', 'updated_at')
 
 admin.site.register(Idea, IdeaModelAdmin)
